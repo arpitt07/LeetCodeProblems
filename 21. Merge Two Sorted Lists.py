c740f1d10f456485ff8df9e6d3c1728f7eab8807
Arpit <patelan2@mcmaster.ca>
2021-09-15 09:50:17 -0400

    completed Merge sorted lists

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -4,5 +4,21 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, l1, l2):
 
+        if not l1 or not l2:
+            return l1 or l2
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+
+#using Recursion method
+# Runtime is O(n+m) as it depends on the length of both l1 and l2. Because each recursive call increments the pointer to l1 or l2 by one (approaching the dangling null at the end of each list),
+
+# there will be exactly one call to mergeTwoLists per element in each list. Therefore, the time complexity is linear in the combined size of the lists.
+
+# Space is also O(n +m) The first call to mergeTwoLists does not return until the ends of both l1 and l2 have been reached, so n + m stack frames consume O(n + m) space.
